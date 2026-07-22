978d79e85ceeca09a4488537b445069484c8f446
BooneG <Boonegg2@gmail.com>
2026-07-21 22:12:05 -0500

    full account functionality

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import json
 from flask import request
 from flask import make_response
 import hashlib
-
+from flask import make_response
 f=open("professors.json","r+")
 data=json.load(f)
 print(data)
@@ -16,34 +16,77 @@ json.dump(data,f)
 from flask import abort, redirect, url_for
 m=hashlib.sha256()
 
-
 @app.route("/")
 
 def root():
-    return '''
+    html='''
+    <dialog>hiii</dialog>
 <p></p>
 <form action ="/professors/search">
    <label for="query"></label>
    <input type="text" id="query" name="qu" placeholder="professor search"><hr>
    <input type="submit" value="Search!">
-</form>
-
+</form>'''
+    if not("accounttoken" in request.cookies) or request.cookies.get("accounttoken")=="0":
+          html+='''
 <form action ="/login">
    <label for="query"></label>
-   <input type="text" id="query" name="u" placeholder="username"><hr>
+   <input type="text" id="query" name="username" placeholder="username"><hr>
 
-   <input type="text" id="query" name="p" placeholder="password"><hr>
-   <input type="submit" value="Search!">
+   <input type="text" id="query" name="password" placeholder="password"><hr>
+   <input type="submit" value="Log in!">
 </form>
+<p>dont have an account? <a href="createaccountpage">make one!</a></p>
 
 '''
+    return html
 
+@app.route('/createaccountpage')
+def makeaccountpage():
+    return '''
+<form action ="/createaccount">
+   <label for="query"></label>
+   <input type="text" id="query" name="username" placeholder="username"><hr>
+
+   <input type="text" id="query" name="password" placeholder="password"><hr>
+   <input type="submit" value="Search!">
+</form>
+'''
+@app.route('/createaccount')
+def makeaccount():
+    tempf=open("users.json","r+")
+    tempdata=json.load(tempf)
+
+    user=request.args.get('username')
+    if not(user in tempdata["users"].keys()):
+        password=request.args.get('password')
+        tempdata["users"][user]=0
+        m.update((user+"buffer"+password).encode('utf-8'))
+        yum=str(m.digest())
+        tempdata["accounts"][yum]={"name":user}
+        tempf.seek(0)
+        json.dump(tempdata,tempf)
+        return "<dialog open="">succesfully created!<a href='/'>go back</a></dialog>"
+    else:
+        return "<dialog open="">user already exists! <a href='/'>go back</a></dialog>"
+
+        
+
+@app.route('/cleartoken')
+def cleart():
+    resp = make_response(redirect(url_for('root')))
+    resp.set_cookie('accounttoken', "0")
+    return resp
 
 @app.route('/login')
 def login():
-    resp = make_response()
-    resp.set_cookie('username', 'the username')
-    return redirect(url_for('root'))
+    user=request.args.get('username')
+    password=request.args.get('password')
+    m.update((user+"buffer"+password).encode('utf-8'))
+    yum=str(m.digest())
+    resp = make_response(redirect(url_for('root')))
+    resp.set_cookie('accounttoken', yum)
+    return resp
 
 
 
@@ -73,16 +116,19 @@ def searched():
 
 @app.route('/professors/profile/<professor>')
 def viewprof(professor):
+    caninteract="accounttoken" in request.cookies and request.cookies.get("accounttoken")!="0"
     f=open("professors.json","r+")
     data=json.load(f)
     html="<p></p><p> </p>"
+    html+=""
     if professor in data.keys():
 
         for filler in data[professor]:
             html+="<p>rating:"+str(filler[0])+"</p><p>"+filler[1]+"</p>"
   
-  
-        html+='''
+        
+        if caninteract:
+            html+='''
 <form action ="/professors/profile/'''+professor+'''/review">
    <label for="query"></label>
    <input type="text" id="query" name="review" placeholder="review"><hr>
@@ -93,13 +139,19 @@ def viewprof(professor):
 
         return f'<p> {escape(professor)}</p>'+html
     else:
-        return '''
-    <p>professor not found! add them? (must be logged in to add professor)</p>
+        html='''<p>professor not found!</p>'''
+        if caninteract:
+            html+='''<p>add them?</p>
 <form action ="/professors/add">
    <label for="query"></label>
-   <input type="text" id="query" name="qu" placeholder="add professor"><hr>
+   <input hidden type="text" id="query" name="qu" value="'''+professor+'''"><hr>
+
    <input type="submit" value="Add!">
 </form>'''
+        else:
+            html+=" (must be logged in to add professor)"
+        return html
+
 
 
 @app.route('/professors/profile/<profname>/review')
